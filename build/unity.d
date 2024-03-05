c93573119b979acd97e58fdb36c578016ff250cb
MatveyMelnikov <matveyy-melnikov@rambler.ru>
2024-03-05 22:17:41 +0300

    Gitignore added

--- build/unity.d
+++
@@ -1,8 +0,0 @@
-build/unity.o: External/Unity-2.5.2/src/unity.c \
- External/Unity-2.5.2/src/unity.h \
- External/Unity-2.5.2/src/unity_internals.h Tests/unity_config.h \
- External/Unity-2.5.2/src/unity.h
-External/Unity-2.5.2/src/unity.h:
-External/Unity-2.5.2/src/unity_internals.h:
-Tests/unity_config.h:
-External/Unity-2.5.2/src/unity.h:
--- build/unity_memory.d
+++
@@ -1,9 +0,0 @@
-build/unity_memory.o: \
- External/Unity-2.5.2/extras/memory/src/unity_memory.c \
- External/Unity-2.5.2/src/unity.h \
- External/Unity-2.5.2/src/unity_internals.h Tests/unity_config.h \
- External/Unity-2.5.2/extras/memory/src/unity_memory.h
-External/Unity-2.5.2/src/unity.h:
-External/Unity-2.5.2/src/unity_internals.h:
-Tests/unity_config.h:
-External/Unity-2.5.2/extras/memory/src/unity_memory.h:
